2f3bc058e8d0e3dfa395514f1adc6e65a43989f9
Sergii <energovodo@gmail.com>
2022-12-23 17:13:01 +0200

    work component as arrow functoin, has many possibilities

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function App() {
+const Title = () => {
+    return <h1>Hello React.js</h1>
+}
+
+const Content = () => {
     return (
         <div>
-            <h1>Hello React.js</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, ab
                 aut soluta officia minus in. Minima totam asperiores, at, id
                 voluptas odio quam autem a reprehenderit dicta hic culpa
                 tenetur.
             </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, ab
+                aut soluta officia minus in. Minima totam asperiores, at, id
+                voluptas odio quam autem a reprehenderit dicta hic culpa
+                tenetur.
+            </p>
+        </div>
+    )
+}
+// стрілкова функція частіше застосовується, записуємо по шаблону, див нижче___
+const App = () => {
+    return (
+        <div>
+            <Title />
+            <Content />
         </div>
     )
 }
