3ef946178b45574fdf5089e70ae3d5e7db667ead
Sergii <energovodo@gmail.com>
2022-12-23 15:49:33 +0200

    work jsx element

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// підхід JSX - це сучасніший метод_____________________
+// const h1 = <h1 id="title" className="test">Hello world</h1>
 
-// const h1 = <h1>Hello world</h1>
+// without jsx, консервативний підхід по якому все працює___
+const h1 = React.createElement(
+    'h1',
+    { id: 'title', className: 'test' },
+    'Hello React'
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{h1}</React.StrictMode>)
