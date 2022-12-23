0a90ab3625de07454a7762718c56e6bc2eaa9ee6
Sergii <energovodo@gmail.com>
2022-12-23 19:30:51 +0200

    relewant and absolut way and add bibliotec MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-const Title = () => {
-    return <h1>Hello React.js</h1>
-}
-
-const Content = () => {
-    return (
-        <>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, ab
-                aut soluta officia minus in. Minima totam asperiores, at, id
-                voluptas odio quam autem a reprehenderit dicta hic culpa
-                tenetur.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, ab
-                aut soluta officia minus in. Minima totam asperiores, at, id
-                voluptas odio quam autem a reprehenderit dicta hic culpa
-                tenetur.
-            </p>
-        </>
-    )
-}
-// стрілкова функція частіше застосовується, записуємо по шаблону, див нижче___
-const App = () => {
-    return (
-        <>
-            <Title />
-            <Content />
-        </>
-    )
-}
+import App from './container/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
