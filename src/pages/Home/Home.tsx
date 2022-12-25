13c143e8ba355be30dcb374bf65cc87a1a76ee79
Sergii <energovodo@gmail.com>
2022-12-25 22:10:45 +0200

    add Home and Main

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+
+type Props = {}
+const Home = (props: Props) => {
+    return (
+        <>
+            <Typography variant="h4" align="center">
+                {' '}
+                List of products{' '}
+            </Typography>
+        </>
+    )
+}
+export default Home
